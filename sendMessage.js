694b85f9bc9bb7766305d49b86fcf3fdf778da0c
androclesleite <androclesleite@gmail.com>
2023-12-21 17:05:46 -0300

    Update sendMessage.js

--- sendMessage.js
+++ sendMessage.js
@@ -1,37 +1,40 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
-const fs = require('fs');
-require('dotenv').config();
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const fs = require("fs");
+require("dotenv").config();
 
-const MENSAGENS_FILE_PATH = './mensagens.json';
+const MENSAGENS_FILE_PATH = "./mensagens.json";
 
 const client = new Client({
-  authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth(),
 });
 
-client.on('qr', qr => {
+client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
-client.on('authenticated', (session) => console.log(`Autenticado`));
+client.on("authenticated", (session) => console.log(`Autenticado`));
 
-client.on('ready', () => {
-  console.log('O LoveNotes está pronto. Não esqueça da estrelinha no repo ⭐ by: Anderson Teala ☕👨🏻‍💻');
-  enviarMensagem(); // Enviar a mensagem para os números especificados
+client.on("ready", () => {
+  console.log(
+    "O LoveNotes está pronto. Não esqueça da estrelinha no repo ⭐ by: Anderson Teala ☕👨🏻‍💻"
+  );
+  enviarMensagem();
 });
 
 client.initialize();
 
-let MensagensBonitas = ['Sua mensagem fixa aqui'];
-
-const phoneNumbers = process.env.PHONE_NUMBERS.split(',');
+let menssage = "Sua mensagem fixa aqui!";
+const phones = "5511920181383,5511984627070";
+const phoneNumbers = phones.split(",");
 
 const enviarMensagem = () => {
   phoneNumbers.forEach(async (phoneNumber) => {
     try {
-      // Enviar a mensagem para o número desejado
-      await client.sendMessage(phoneNumber, MensagensBonitas[0]); // Envie a única mensagem definida
-      console.log(`Mensagem enviada para ${phoneNumber}: ${MensagensBonitas[0]}`);
+      await client.enviarMensagem(phoneNumber, menssage); 
+      console.log(
+        `Mensagem enviada para ${phoneNumber}: ${menssage}`
+      );
     } catch (error) {
       console.log(`Erro ao enviar mensagem para ${phoneNumber}:`, error);
     }
